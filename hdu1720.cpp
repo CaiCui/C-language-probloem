3d08f1ce263da026f9d5ed487f4a08c06d174136
崔崔崔 <buaacyf@126.com>
2016-10-17 21:20:41 +0800

    Add files via upload

---
+++ hdu1720.cpp
@@ -0,0 +1,16 @@
+//hdu1720
+//%X大写16进制输出 %x小写16进制输出  %p读入16进制数
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int a,b;
+    while(scanf("%p%p",&a,&b)!=EOF)//scanf("%x%x",&a,&b)==2
+    {
+         printf("%d\n",a+b);
+    }
+
+    return 0;
+}
